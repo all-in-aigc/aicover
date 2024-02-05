8b4e68d44b511f01fe98cca26d6aea9c2491335f
idoubi <me@idoubi.cc>
2024-02-05 15:32:57 +0800

    only allow wechat pay

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -93,7 +93,7 @@ export async function POST(req: Request) {
     };
 
     if (currency === "cny") {
-      options.payment_method_types = ["wechat_pay", "card"];
+      options.payment_method_types = ["wechat_pay"];
       options.payment_method_options = {
         wechat_pay: {
           client: "web",
